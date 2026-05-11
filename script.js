d358bd1b8b1c615e09272f0489e41c85dcb99b16
nurazizahtuzzahroh-rgb <nurazizahtuzzahroh@gmail.com>
2026-05-12 06:12:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,12 +16,3 @@ const revealOnScroll = () => {
 window.addEventListener('scroll', revealOnScroll);
 window.addEventListener('load', revealOnScroll);
 
-// Ketika menu mobile dibuka, tampilkan/tutup menu.
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.main-nav');
-
-if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('open');
-  });
-}
